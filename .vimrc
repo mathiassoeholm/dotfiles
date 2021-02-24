2b4dd41721e84a246ef15c124c3251e0628733c2
Mathias Soeholm <mathiassoeholm@gmail.com>
2021-02-24 07:33:39 +0100

    Keep VIM swap files in one dir

--- .vimrc
+++ .vimrc
@@ -1,3 +1,5 @@
 " esc in insert mode
 inoremap kj <esc>
 
+" keep all swap files in the same folder (https://webdevetc.com/blog/should-you-disable-vims-swap-files-swp-being-created/)
+set directory^=$HOME/vimswap//
--- misc-setup.sh
+++ misc-setup.sh
@@ -1,2 +1,5 @@
 # Enable key repeating in VS Code (https://github.com/VSCodeVim/Vim#mac)
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+
+# Create a directory to place VIM swap files
+mkdir ~/vimswap/
