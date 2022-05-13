7b4595e9d9c67ca40b5511080a57758f4ff7e704
Morten <morten@setsnail.com>
2022-05-13 16:44:26 +0200

    Added stylua for formatting of lua files

---
+++ lua/configs/stylua.lua
@@ -0,0 +1,13 @@
+-- TODO Add this when lspconfig has been setup
+
+-- local lsp_config = require("lspconfig")
+-- lsp_config.sumneko_lua.setup({
+--     commands = {
+--       Format = {
+--         function()
+--           require("stylua-nvim").format_file()
+--         end,
+--       },
+--     },
+--     ...
+-- })
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,6 +2,11 @@ return require('packer').startup(function()
 	-- Packer can manage itself
 	use 'wbthomason/packer.nvim'
 
+    -- Like Prettier but styling for Lua
+    use({"ckipp01/stylua-nvim"})
+    require('configs.stylua')
+
+    -- Tabs and Buffers
 	use {'akinsho/bufferline.nvim', tag = "v2.*", requires = 'kyazdani42/nvim-web-devicons'}
     require('configs.bufferline')
 
