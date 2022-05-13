014a43bfa00e159137997ac2f6759a84a64aacc5
Morten <morten@setsnail.com>
2022-05-13 15:45:33 +0200

    Added packer as package manager

---
+++ lua/basic.lua
@@ -0,0 +1,12 @@
+print('hello from basic');
+local set = vim.opt
+
+-- use space as a the leader key
+vim.g.mapleader = ' '
+
+-- Set the behavior of tab
+set.tabstop = 4
+set.shiftwidth = 4
+set.softtabstop = 4
+set.expandtab = true
+
---
+++ lua/plugins.lua
@@ -0,0 +1,7 @@
+return require('packer').startup(function()
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
+
+  use {'akinsho/bufferline.nvim', tag = "v2.*", requires = 'kyazdani42/nvim-web-devicons'}
+
+end)
