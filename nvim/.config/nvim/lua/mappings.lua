8dc75d1aca839e35e30c87a8065055f90d78deb7
Morten <morten@setsnail.com>
2022-05-17 10:42:30 +0200

    Added and moved some CoC keybindings

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -31,3 +31,21 @@ vim.keymap.set("n", "<leader>m", ":Format<CR>")
 
 -- File explorer
 vim.keymap.set("n", "<C-n>", ":NvimTreeFindFileToggle<CR>")
+
+-- CoC specific bindings
+vim.api.nvim_set_keymap("n", "<leader>.", "<Plug>(coc-codeaction)", {})
+vim.api.nvim_set_keymap("n", "<leader>l", ":CocCommand eslint.executeAutofix<CR>", {})
+vim.api.nvim_set_keymap("n", "gd", "<Plug>(coc-definition)", {silent = true})
+vim.api.nvim_set_keymap("n", "<C-s>", "<Plug>(coc-range-select)", {silent = true})
+vim.api.nvim_set_keymap("x", "<C-s>", "<Plug>(coc-range-select)", {silent = true})
+vim.api.nvim_set_keymap("n", "K", ":call CocActionAsync('doHover')<CR>", {silent = true, noremap = true})
+vim.api.nvim_set_keymap("n", "<leader>rn", "<Plug>(coc-rename)", {})
+vim.api.nvim_set_keymap("n", "<leader>m", ":CocCommand prettier.formatFile<CR>", {noremap = true})
+vim.api.nvim_set_keymap("i", "<C-Space>", "coc#refresh()", { silent = true, expr = true })
+vim.api.nvim_set_keymap("i", "<TAB>", "pumvisible() ? '<C-n>' : '<TAB>'", {noremap = true, silent = true, expr = true})
+vim.api.nvim_set_keymap("i", "<S-TAB>", "pumvisible() ? '<C-p>' : '<C-h>'", {noremap = true, expr = true})
+vim.api.nvim_set_keymap("i", "<CR>", "pumvisible() ? coc#_select_confirm() : '<C-G>u<CR><C-R>=coc#on_enter()<CR>'", {silent = true, expr = true, noremap = true})
+
+-- Copilot
+vim.keymap.set("i", "<C-l>", 'copilot#Accept()', {silent = true, expr = true, script = true})
+vim.g.copilot_no_tab_map = 1
