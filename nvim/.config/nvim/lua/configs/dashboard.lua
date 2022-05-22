bd28ad569e9e696341f4447bc9254877a824c8e2
Morten <morten@setsnail.com>
2022-05-22 20:30:07 +0200

    added ASCII snail and some clean up

--- nvim/.config/nvim/lua/configs/dashboard.lua
+++ nvim/.config/nvim/lua/configs/dashboard.lua
@@ -1,4 +1,23 @@
-vim.g.dashboard_default_executive ='telescope'
+vim.g.dashboard_default_executive = 'telescope'
 
 
+--     .----.   @   @
+--    / .-"-.`.  \v/
+--    | | '\ \ \_/ )
+--  ,-\ `-.' /.'  /
+-- '---`----'----'
+vim.g.dashboard_custom_header = { 
+    '     .----.   @   @',
+    '    / .-"-.`.  \\v/',
+    "    | | '\\ \\ \\_/ )",
+    "  ,-\\ `-.' /.'  /",
+    " '---`----'----'"
+}
 
+-- disable the indenLine while dashboard is open
+vim.g.indentLine_fileTypeExclude = { 'dashboard' }
+
+
+vim.g.dashboard_custom_footer = {
+   "Slow and steady wins the race",
+}
