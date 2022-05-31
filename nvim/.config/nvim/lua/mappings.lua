5496a7092220957b4a2bd6bf7d87ce80d58f47fb
Morten <morten@setsnail.com>
2022-05-31 23:55:09 +0200

    flipped resice arrow keys

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -17,10 +17,10 @@ vim.keymap.set("n", "<C-k>", "<C-w>k")
 vim.keymap.set("n", "<C-l>", "<C-w>l")
 
 -- Resize buffers
-vim.keymap.set("n", "<C-Left>", ":vertical resize +3<CR>")
-vim.keymap.set("n", "<C-Right>", ":vertical resize -3<CR>")
-vim.keymap.set("n", "<C-Up>", ":resize +3<CR>")
-vim.keymap.set("n", "<C-Down>", ":resize -3<CR>")
+vim.keymap.set("n", "<C-Left>", ":vertical resize -3<CR>")
+vim.keymap.set("n", "<C-Right>", ":vertical resize +3<CR>")
+vim.keymap.set("n", "<C-Up>", ":resize -3<CR>")
+vim.keymap.set("n", "<C-Down>", ":resize +3<CR>")
 
 -- Move buffer
 vim.keymap.set("n", "<S-l>", ":BufferLineCycleNext<CR>")
