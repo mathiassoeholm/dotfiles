56d20c589fde630da40a7492ccd03151b4286812
Morten <morten@setsnail.com>
2022-07-04 18:49:38 +0200

    Cleanup themes

--- nvim/.config/nvim/lua/extra.lua
+++ nvim/.config/nvim/lua/extra.lua
@@ -1,40 +1,55 @@
 -- Keybing for toggleing favourite themes
 local currentTheme = 2
+local function setColorMode(mode)
+	vim.opt.background = mode
+
+	local themePath
+	if mode == "dark" then
+		themePath = string.format(vim.env.HOME .. "/.config/kitty/catpuccino-theme.conf")
+	else
+		themePath = string.format(vim.env.HOME .. "/.config/kitty/catpuccino-theme-light.conf")
+	end
+
+	-- TODO change kitty terminal color scheme as well to change the cursor
+	-- handle = vim.loop.spawn("kitty", {
+	-- 	args = {
+	-- 		"@",
+	-- 		"set-colors",
+	-- 		"-c",
+	-- 		themePath,
+	-- 	},
+	-- }, function()
+	-- 	print("DOCUMENT CONVERSION COMPLETE")
+	-- 	handle:close()
+	-- end)
+end
+
 function ToggleTheme()
 	currentTheme = (currentTheme + 1) % 6
 	if currentTheme == 0 then
-		vim.opt.background = "light"
+		setColorMode("light")
 		vim.cmd([[colorscheme gruvbox]])
 	elseif currentTheme == 1 then
-		vim.opt.background = "dark"
+		setColorMode("dark")
 		vim.cmd([[colorscheme gruvbox]])
 	elseif currentTheme == 2 then
+		setColorMode("dark")
 		vim.g.catppuccin_flavour = "mocha"
-		vim.opt.background = "dark"
 		vim.cmd([[colorscheme catppuccin]])
 	elseif currentTheme == 3 then
+		setColorMode("dark")
 		vim.g.catppuccin_flavour = "macchiato"
-		vim.opt.background = "dark"
 		vim.cmd([[colorscheme catppuccin]])
 	elseif currentTheme == 4 then
+		setColorMode("dark")
 		vim.g.catppuccin_flavour = "frappe"
-		vim.opt.background = "dark"
 		vim.cmd([[colorscheme catppuccin]])
 	elseif currentTheme == 5 then
+		setColorMode("light")
 		vim.g.catppuccin_flavour = "latte"
-		vim.opt.background = "light"
 		vim.cmd([[colorscheme catppuccin]])
 	end
 
-    -- TODO change kitty terminal color scheme as well to change the cursor
-    -- vim.loop.spawn('kitty', {
-    --     args = {
-    --         '@',
-    --         'set-colors',
-    --         '-c',
-    --         string.format(vim.env.HOME..'/base16-kitty/colors/%s.conf', name)
-    --     }
-    -- }, nil)
 end
 
 vim.api.nvim_set_keymap("n", "<C-T>", ":lua ToggleTheme()<CR>", { silent = true })
