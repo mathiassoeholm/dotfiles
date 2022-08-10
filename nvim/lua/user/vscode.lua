b4ef0b3ad76f70d138765b8416a5712e3589fc9a
Mathias Soeholm <mathiassoeholm@gmail.com>
2022-08-10 21:21:26 +0200

    More VS Code key binding

--- nvim/lua/user/vscode.lua
+++ nvim/lua/user/vscode.lua
@@ -6,3 +6,13 @@ local keymap = vim.api.nvim_set_keymap
 
 keymap("n", "<space>", ':call VSCodeNotify("whichkey.show")<CR>', opts)
 keymap("x", "<space>", ':call VSCodeNotify("whichkey.show")<CR>', opts)
+
+-- Better Navigation
+keymap("n", "<C-j>", ":call VSCodeNotify('workbench.action.navigateDown')<CR>", opts)
+keymap("x", "<C-j>", ":call VSCodeNotify('workbench.action.navigateDown')<CR>", opts)
+keymap("n", "<C-k>", ":call VSCodeNotify('workbench.action.navigateUp')<CR>", opts)
+keymap("x", "<C-k>", ":call VSCodeNotify('workbench.action.navigateUp')<CR>", opts)
+keymap("n", "<C-h>", ":call VSCodeNotify('workbench.action.navigateLeft')<CR>", opts)
+keymap("x", "<C-h>", ":call VSCodeNotify('workbench.action.navigateLeft')<CR>", opts)
+keymap("n", "<C-l>", ":call VSCodeNotify('workbench.action.navigateRight')<CR>", opts)
+keymap("x", "<C-l>", ":call VSCodeNotify('workbench.action.navigateRight')<CR>", opts)
