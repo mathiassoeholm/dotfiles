7b9c6548fbd69253db6196ea15c5b2ac8d58bfdd
Mathias Soeholm <mathiassoeholm@gmail.com>
2022-08-12 15:03:45 +0200

    Config update

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -3,16 +3,10 @@ local fn = vim.fn
 -- Automatically install packer
 local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  }
-  print "Installing packer close and reopen Neovim..."
-  vim.cmd [[packadd packer.nvim]]
+    PACKER_BOOTSTRAP = fn.system {"git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim",
+                                  install_path}
+    print "Installing packer close and reopen Neovim..."
+    vim.cmd [[packadd packer.nvim]]
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins.lua file
@@ -26,48 +20,70 @@ vim.cmd [[
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
-  return
+    return
 end
 
 -- Have packer use a popup window
 packer.init {
-  display = {
-    open_fn = function()
-      return require("packer.util").float { border = "rounded" }
-    end,
-  },
+    display = {
+        open_fn = function()
+            return require("packer.util").float {
+                border = "rounded"
+            }
+        end
+    }
 }
+
+local function is_vscode()
+    return vim.g.vscode
+end
+
+local function is_not_vscode()
+    return not vim.g.vscode
+end
+
+print(is_not_vscode())
 
 -- Install your plugins here
 return packer.startup(function(use)
-  use "wbthomason/packer.nvim" -- Have packer manage itself
+    function nvimPlugin(name)
+        use {
+            name,
+            cond = is_not_vscode
+        }
+    end
+    function vscodePlugin(name)
+        use {
+            name,
+            cond = is_vscode
+        }
+    end
 
-  -- Many plugins rely on these two plugins, so they are just here if needed
-  use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
-  use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+    use "wbthomason/packer.nvim" -- Have packer manage itsel
 
-  -- Colorschemes 
-  use "lunarvim/darkplus.nvim"
+    vscodePlugin "asvetliakov/vim-easymotion"
 
-  -- cmp plugins
-  use "hrsh7th/nvim-cmp" -- The completion plugin
-  use "hrsh7th/cmp-buffer" -- buffer completions
-  use "hrsh7th/cmp-path" -- path completions
-  use "hrsh7th/cmp-cmdline" -- cmdline completions
-  use "saadparwaiz1/cmp_luasnip" -- snippet completions
+    -- Many plugins rely on these two plugins, so they are just here if needed
+    nvimPlugin "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
+    nvimPlugin "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
 
-  -- snippets
-  use "L3MON4D3/LuaSnip" --snippet engine
-  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use use "lunarvim/darkplus.nvim"
+    -- Colorschemes 
+    nvimPlugin "lunarvim/darkplus.nvim"
 
-  -- LSP
-  use "neovim/nvim-lspconfig" -- enable LSP
-  use "williamboman/mason-lspconfig.nvim" -- Bridge masing and lspconfig
-  use "williamboman/mason.nvim" -- Easily install and manage LSP servers
+    -- cmp plugins
+    nvimPlugin "hrsh7th/nvim-cmp" -- The completion plugin
+    nvimPlugin "hrsh7th/cmp-buffer" -- buffer completions
+    nvimPlugin "hrsh7th/cmp-path" -- path completions
+    nvimPlugin "hrsh7th/cmp-cmdline" -- cmdline completions
+    nvimPlugin "saadparwaiz1/cmp_luasnip" -- snippet completions
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if PACKER_BOOTSTRAP then
-    require("packer").sync()
-  end
+    -- snippets
+    nvimPlugin "L3MON4D3/LuaSnip" -- snippet engine
+    nvimPlugin "rafamadriz/friendly-snippets" -- a bunch of snippets to use use "lunarvim/darkplus.nvim"
+
+    -- Automatically set up your configuration after cloning packer.nvim
+    -- Put this at the end after all plugins
+    if PACKER_BOOTSTRAP then
+        require("packer").sync()
+    end
 end)
