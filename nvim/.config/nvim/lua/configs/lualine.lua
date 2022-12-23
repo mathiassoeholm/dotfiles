eccfd8203f1885ccab9ea504840011734ba44466
Morten <morten@setsnail.com>
2022-12-23 16:23:32 +0100

    Added macro info to the lualine

--- nvim/.config/nvim/lua/configs/lualine.lua
+++ nvim/.config/nvim/lua/configs/lualine.lua
@@ -11,8 +11,14 @@ M.Compile = function(packerUse)
 					lualine_a = { "mode" },
 					lualine_b = { "branch" },
 					lualine_c = { "filename" },
-					lualine_x = { "filetype" },
-					lualine_y = { "progress" },
+					lualine_x = {
+						{
+							require("noice").api.statusline.mode.get,
+							cond = require("noice").api.statusline.mode.has,
+							color = { fg = "#ff9e64" },
+						}
+					},
+					lualine_y = { "filetype" },
 					lualine_z = { "location" },
 				},
 				inactive_sections = {
