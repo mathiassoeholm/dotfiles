743e744a3d657829081541662c49074c76227aee
Erik Høj Petersen <erik@setsnail.com>
2023-02-02 21:42:58 +0100

    files now autoreload when vim gains focus when exiting C+f

--- nvim/.config/nvim/lua/basic.lua
+++ nvim/.config/nvim/lua/basic.lua
@@ -13,6 +13,15 @@ set.cmdheight = 0
 
 -- disable swap files
 vim.cmd("noswapfile")
+vim.opt.swapfile = false
+
+-- Automatically reload files when they change on disk without having any edited buffers of that file open.
+-- Taken from: https://neovim.discourse.group/t/a-lua-based-auto-refresh-buffers-when-they-change-on-disk-function/2482/3
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = { "*" },
+})
 
 -- Set the behavior of tab
 set.autoindent = true
