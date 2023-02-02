2bec7deffcb7d5e675b282624a16f46f9f265898
Erik Høj Petersen <erik@setsnail.com>
2023-02-02 21:51:25 +0100

    cleanup

--- nvim/.config/nvim/lua/basic.lua
+++ nvim/.config/nvim/lua/basic.lua
@@ -12,12 +12,11 @@ set.mouse = "a"
 set.cmdheight = 0
 
 -- disable swap files
-vim.cmd("noswapfile")
-vim.opt.swapfile = false
+set.swapfile = false
 
 -- Automatically reload files when they change on disk without having any edited buffers of that file open.
 -- Taken from: https://neovim.discourse.group/t/a-lua-based-auto-refresh-buffers-when-they-change-on-disk-function/2482/3
-vim.o.autoread = true
+set.autoread = true
 vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
   command = "if mode() != 'c' | checktime | endif",
   pattern = { "*" },
@@ -30,8 +29,6 @@ set.tabstop = 4
 set.shiftwidth = 4
 set.softtabstop = 4
 set.expandtab = false -- Tabs over spaces
-
-set.swapfile = false
 
 -- Numbers
 set.relativenumber = true
