f2d2f1b523e78d76fcc02f57b030396bdd35b2a7
Erik Høj Petersen <erik@setsnail.com>
2023-02-04 18:16:04 +0100

    added .env. Now setting mouse acceleration based on .env

--- scripts/stow-ignore/mac-setup.sh
+++ scripts/stow-ignore/mac-setup.sh
@@ -1,2 +1,14 @@
+# Load the .env file and export all the environment variables
+export $(grep -v '^#' .env | xargs)
+
 # Disable the dock animation, to make it show/hide instantly
 defaults write com.apple.dock autohide-time-modifier -int 0;killall Dock
+
+if [ $MOUSE_ACCELERATION = "true" ]
+then
+	echo "Enabled mouse acceleration"
+	defaults write .GlobalPreferences com.apple.mouse.scaling 1
+else 
+	echo "Disabled mouse acceleration"
+	defaults write .GlobalPreferences com.apple.mouse.scaling -1
+fi
