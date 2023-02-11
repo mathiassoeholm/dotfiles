3f5316a680456cf36a5c6bb71aa504ff324c4e71
Mathias Soeholm <mathias@Mathiass-MacBook-Pro.local>
2023-02-11 15:36:03 +0100

    mathias

--- scripts/stow-ignore/first-time-setup.sh
+++ scripts/stow-ignore/first-time-setup.sh
@@ -12,6 +12,7 @@ eval "$(ssh-agent -s)"
 
 # Install Homebrew (this also installs git)
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 git clone git@github.com:DeepPlateMC/dotfiles.git ~/dotfiles
 
