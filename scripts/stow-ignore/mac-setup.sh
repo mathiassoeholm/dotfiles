a48eadfc960e473451f83e4ea7f2c5b56a3cbb7c
Erik Høj Petersen <erik@setsnail.com>
2023-02-11 16:53:40 +0100

    wip

--- scripts/stow-ignore/mac-setup.sh
+++ scripts/stow-ignore/mac-setup.sh
@@ -27,6 +27,11 @@ else
 	defaults write .GlobalPreferences com.apple.mouse.scaling -1
 fi
 
+# Install zsh plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/agkozak/zsh-z ~/.oh-my-zsh/custom/plugins/zsh-z
+
 eval "$(/opt/homebrew/bin/brew shellenv)"
 brew update
 
