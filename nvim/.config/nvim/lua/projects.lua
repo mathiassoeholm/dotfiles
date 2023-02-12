1595943777a5c26b8d1a126d4cd509782ca22860
Mathias <mathiassoeholm@gmail.com>
2023-02-12 13:55:26 +0100

    erithias

--- nvim/.config/nvim/lua/projects.lua
+++ nvim/.config/nvim/lua/projects.lua
@@ -19,25 +19,36 @@ end
 
 local buildFunction
 if activeProject ~= nil then
-	local spinner_frames = { "⣾", "⣽", "⣻", "⢿", "⡿", "⣟", "⣯", "⣷" }
-
 	buildFunction = function()
 		if string.find(currentPath, "project-grim", nil, true) then
 			local notification
-			local done = false
+			local state = "running"
 			local spinner_index = 1
+			local error = ""
+
+			local spinner_frames = { "⣾", "⣽", "⣻", "⢿", "⡿", "⣟", "⣯", "⣷" }
+			local function get_notify_config()
+				if state == "running" then
+					return { type = "info", title = "Building", icon = spinner_frames[spinner_index] }
+				elseif state == "success" then
+					return { type = "info", title = "Successfully Built", icon = "" }
+				elseif state == "failure" then
+					return { type = "error", title = "Build Failed", icon = "", body = error, timeout = 5000 }
+				end
+			end
 
 			local function update_notification()
-				notification = vim.notify("", "info", {
-					title = "Pushing code",
-					timeout = 1000,
-					icon = done and "" or spinner_frames[spinner_index],
+				local config = get_notify_config()
+				notification = vim.notify(config.body or "", config.type, {
+					title = config.title,
+					timeout = config.timeout or 1000,
+					icon = config.icon,
 					replace = notification,
 				})
 
 				spinner_index = (spinner_index + 1) % #spinner_frames
 
-				if not done then
+				if state == "running" then
 					vim.defer_fn(update_notification, 100)
 				end
 			end
@@ -45,8 +56,12 @@ if activeProject ~= nil then
 			update_notification()
 
 			vim.fn.jobstart("yarn grim push:code:wait", {
-				on_exit = function()
-					done = true
+				on_exit = function(_, code)
+					state = code == 0 and "success" or "failure"
+				end,
+				stdout_buffered = true,
+				on_stderr = function(_, data)
+					error = error .. table.concat(data, "\n")
 				end,
 			})
 		end
