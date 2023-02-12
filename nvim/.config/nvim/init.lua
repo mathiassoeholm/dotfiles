2dcaeda9f65a8b9a0f14e5c30376df1256e25506
Erik <erikhoj.p@gmail.com>
2023-02-12 21:04:37 +0100

    erik!

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -5,4 +5,7 @@ require("default-mappings")
 require("theming")
 require("projects")
 
-require("trouble-extensions")
+local trouble_extensions = require("trouble-extensions")
+vim.api.nvim_create_user_command("Diagnose", trouble_extensions.diagnose, {})
+
+vim.api.nvim_set_keymap("n", "<leader>di", ":Diagnose<CR>", { noremap = true, silent = true })
--- nvim/.config/nvim/lua/trouble-extensions.lua
+++ nvim/.config/nvim/lua/trouble-extensions.lua
@@ -3,23 +3,22 @@ local trouble_util = require("trouble.util")
 
 local M = {}
 
-function M.tsc(_, _, cb, _)
-	local tsConfigPath = vim.fn.findfile("tsconfig.json", ".;")
-
-	if tsConfigPath == "" then
-		print("No tsconfig.json found, cannot type-check")
+local function gather_tsc_results(results, callback)
+	if M.ts_config_path == "" then
+		vim.notify(
+			"No tsconfig.json file found in path of current buffer, cannot type-check",
+			"error",
+			{ title = "Failed to Type-check" }
+		)
 		return
 	end
 
-	local command = "yarn --silent tsc --noEmit --pretty false --project " .. tsConfigPath .. " | tsc-output-parser"
+	local command = "yarn --silent tsc --noEmit --pretty false --project " .. M.ts_config_path .. " | tsc-output-parser"
 
 	util.run_job_with_notification({
 		command = command,
 
 		callback = function(output)
-			print(output)
-
-			local items = {}
 			local rawItems = vim.json.decode(output)
 			for _, error in ipairs(rawItems) do
 				local item = {
@@ -33,10 +32,10 @@ function M.tsc(_, _, cb, _)
 					code = error.value.tsError.value.errorString,
 				}
 
-				table.insert(items, trouble_util.process_item(item))
+				table.insert(results, trouble_util.process_item(item))
 			end
 
-			cb(items)
+			callback(results)
 		end,
 
 		title = function(state)
@@ -51,56 +50,77 @@ function M.tsc(_, _, cb, _)
 	})
 end
 
-function M.eslint(_, buf, cb, options)
-	local eslintConfigPath = vim.fn.findfile(".eslintrc.cjs", ".;")
-
-	if eslintConfigPath == "" then
-		print("No .eslintrc.cjs found, cannot lint")
+local function gather_eslint_results(results, callback)
+	if M.eslint_config_path == "" then
+		vim.notify(
+			"No .eslintrc.cjs file found in path of current buffer, cannot lint",
+			"error",
+			{ title = "Failed to Lint" }
+		)
 		return
 	end
 
-	local directory
-	for dir in vim.fs.parents(eslintConfigPath) do
-		directory = dir
-	end
+	local directory = vim.fn.fnamemodify(M.eslint_config_path, ":h")
 
 	local command = "yarn --silent eslint -f json " .. directory
 
-	local handle = io.popen(command)
-	if handle == nil then
-		print("Unable to start command: " .. command)
-		return
-	end
+	util.run_job_with_notification({
+		command = command,
 
-	local result = handle:read("*a")
-	handle:close()
+		callback = function(output)
+			local files = vim.json.decode(output)
 
-	local items = {}
-	local files = vim.json.decode(result)
+			for _, file in ipairs(files) do
+				for _, message in ipairs(file.messages) do
+					local item = {
+						bufnr = vim.fn.bufnr(file.filePath, true),
+						lnum = message.line - 1,
+						col = message.column - 1,
+						end_lnum = message.endLine - 1,
+						end_col = message.endColumn - 1,
+						severity = message.severity,
+						message = message.message,
+						code = message.ruleId,
+					}
 
-	for _, file in ipairs(files) do
-		for _, message in ipairs(file.messages) do
-			local item = {
-				bufnr = vim.fn.bufnr(file.filePath, true),
-				lnum = message.line - 1,
-				col = message.column - 1,
-				end_lnum = message.endLine - 1,
-				end_col = message.endColumn - 1,
-				severity = message.severity,
-				message = message.message,
-				code = message.ruleId,
-			}
+					table.insert(results, trouble_util.process_item(item))
+				end
+			end
 
-			table.insert(items, trouble_util.process_item(item))
-		end
-	end
+			callback(results)
+		end,
 
-	cb(items)
+		title = function(state)
+			if state == "success" then
+				return "Successfully Linted"
+			elseif state == "failure" then
+				return "Failed to Lint"
+			end
+
+			return "Linting"
+		end,
+	})
 end
 
-M.tsc_and_eslint = function() end
+function M.diagnose()
+	-- Cache the tsconfig and eslint config paths before the Trouble buffer opens.
+	local buffer_path = vim.fn.expand("%:p")
+	M.ts_config_path = vim.fn.findfile("tsconfig.json", buffer_path .. ".;")
+	M.eslint_config_path = vim.fn.findfile(".eslintrc.cjs", buffer_path .. ".;")
+
+	-- Open Trouble and start result gathering
+	require("trouble").open("tsc_eslint")
+end
+
+M.tsc_and_eslint = function(_, _, callback, _)
+	local results = {}
+
+	-- Start result gathering
+	gather_tsc_results(results, callback)
+	gather_eslint_results(results, callback)
+end
 
 local trouble_providers = require("trouble.providers").providers
-trouble_providers.tsc = M.tsc
+trouble_providers.tsc_eslint = M.tsc_and_eslint
 
 return M
