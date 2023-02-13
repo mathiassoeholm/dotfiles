dd0bd10aee8cfcd26057d64ef9124b0df6e0a984
Claussen <morten@setsnail.com>
2023-02-13 09:38:15 +0100

    Morten

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -4,8 +4,3 @@ require("default-mappings")
 
 require("theming")
 require("projects")
-
-local trouble_extensions = require("trouble-extensions")
-vim.api.nvim_create_user_command("Diagnose", trouble_extensions.diagnose, {})
-
-vim.api.nvim_set_keymap("n", "<leader>di", ":Diagnose<CR>", { noremap = true, silent = true })
--- nvim/.config/nvim/lua/configs/trouble.lua
+++ nvim/.config/nvim/lua/configs/trouble.lua
@@ -6,8 +6,15 @@ M.Compile = function(packerUse)
 		requires = "nvim-tree/nvim-web-devicons",
 		config = function()
 			require("trouble").setup({})
+
+			local trouble_extensions = require("trouble-extensions")
+			vim.api.nvim_create_user_command("Diagnose", trouble_extensions.diagnose, {})
 		end,
 	})
+end
+
+M.Added = function()
+	vim.api.nvim_set_keymap("n", "<leader>di", ":Diagnose<CR>", { noremap = true, silent = true })
 end
 
 return M
